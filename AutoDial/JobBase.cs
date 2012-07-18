2b841832ce36301147f7b4f4df4c0f9d525305cc
lurongkai <lurongkai@gmail.com>
2012-07-18 14:58:02 +0800

    init.

---
+++ AutoDial/JobBase.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Quartz;
+using System.Text;
+
+namespace AutoDial
+{
+    public abstract class JobBase
+    {
+        public void ExecuteWapper(IJobExecutionContext context) {
+            Console.WriteLine(context.JobDetail.Key.Name + ": " + DateTime.Now);
+            var addressString = GetAddressesString();
+            ExecuteCore(addressString);
+        }
+
+        protected abstract void ExecuteCore(string addresses);
+
+        protected virtual void ExecuteExcetion(Exception e) {
+            Util.Logger.Fatal("Job execute failure.", e);
+        }
+
+        private string GetAddressesString() {
+            var addresses = GetLocalInterfaceAddresses()
+                .Where(add => !add.ToString().StartsWith("192"))
+                .Select(add => add.ToString())
+                .OrderBy(s => s);
+            var sb = new StringBuilder();
+            addresses.ToList().ForEach(add => sb.Append(add + "|"));
+            return sb.ToString();
+        }
+
+        public IEnumerable<IPAddress> GetLocalInterfaceAddresses() {
+            var hostName = Dns.GetHostName();
+            var addresses = Dns.GetHostAddresses(hostName);
+            var ipv4Address = addresses
+                .Where(ip => !ip.IsIPv6LinkLocal)
+                .OrderBy(add => add.GetHashCode());
+            return ipv4Address;
+        }
+    }
+}
---
+++ AutoDial/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Quartz;
+using Quartz.Impl;
+using AMicroblogAPI;
+using AMicroblogAPI.Common;
+
+namespace AutoDial
+{
+    class Program
+    {
+        private static OAuthAccessToken _token = WeiboLogin();
+
+        static void Main(string[] args) {
+            var sf = new StdSchedulerFactory();
+            var scheduler = sf.GetScheduler();
+
+            if (Properties.Settings.Default.EnableEmail) {
+                BuildEmailJob(scheduler);
+            }
+            if (Properties.Settings.Default.EnableWeibo) {
+                BuildWeiboJob(scheduler);
+            }
+
+            scheduler.Start();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            scheduler.Shutdown();
+        }
+
+        private static void BuildEmailJob(IScheduler scheduler) {
+            var jobDetail = JobBuilder.Create<SendIpByEmail>()
+                .WithIdentity("Send_IP_Email")
+                .Build();
+            var trigger = TriggerBuilder.Create()
+                .WithSimpleSchedule(ssb =>
+                    ssb.WithIntervalInMinutes(Properties.Settings.Default.EmailIntervalByMinute)
+                                               .RepeatForever())
+                .Build();
+
+            scheduler.ScheduleJob(jobDetail, trigger);
+        }
+
+        private static void BuildWeiboJob(IScheduler scheduler) {
+            var jobDetail = JobBuilder.Create<SendIpByWeibo>()
+                .WithIdentity("Send_IP_Weibo")
+                .Build();
+            var trigger = TriggerBuilder.Create()
+                .WithSimpleSchedule(ssb =>
+                    ssb.WithIntervalInMinutes(Properties.Settings.Default.WeiboIntervalByMinute)
+                                               .RepeatForever())
+                .Build();
+
+            scheduler.ScheduleJob(jobDetail, trigger);
+        }
+
+        private static OAuthAccessToken WeiboLogin() {
+            OAuthAccessToken user = null;
+            for (int i = 0; i < 10; i++) {
+                try {
+                    user = AMicroblog.Login(Properties.Settings.Default.WeiboUsername, Properties.Settings.Default.WeiboPassword);
+                } catch (Exception e) {
+                    Util.Logger.Fatal("Login failure.", e);
+                    continue;
+                }
+                break;
+            }
+            return user;
+        }
+    }
+}
