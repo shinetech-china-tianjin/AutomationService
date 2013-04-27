add028724e5824fa386562398c755390b3790c4f
Lu Rongkai <lurongkai@gmail.com>
2013-04-27 10:33:17 +0800

    modified:   src/Shinetech.TianJin.AutomationService.SlotInterface/Acivation/ActivationDescription.cs

--- src/Shinetech.TianJin.AutomationService.SlotInterface/Acivation/ActivationDescription.cs
+++ src/Shinetech.TianJin.AutomationService.SlotInterface/Acivation/ActivationDescription.cs
@@ -7,5 +7,7 @@ namespace Shinetech.TianJin.AutomationService.SlotInterface
 {
     public class ActivationDescription
     {
+		DateTime ActivationTime { get; private set;}
+		string SlotName { get; privat set; }
     }
 }
